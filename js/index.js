21dbc11981256a9df1b88a59d4312bb48e8a146b
Aaron Baker <aarbaker@coca-cola.com>
2017-06-12 22:01:42 -0400

    brag that init

--- js/index.js
+++ js/index.js
@@ -1,83 +1,73 @@
+var s = {};
+var gauge;
+
   var homepage = new Vue({
   el: '#target',
   data: function(){
 
     return {
-      s:s,//s is for app State
-      docButtonHover: false,
-      searchButtonHover: false,
-      searchQuery: null
+      s:s//s is for app State
     }
 
   },
   mounted: function() {
 
     console.log("mounted");
+    Vue.config.devtools = true;
 
-    var elem = document.querySelector('.main-carousel');
-    var flkty = new Flickity( elem, {
-      cellAlign: 'left',
-      wrapAround: true,
-      contain: true
-    });
+    var opts = {
+      angle: 0.01, // The span of the gauge arc
+      lineWidth: 0.07, // The line thickness
+      radiusScale: 1, // Relative radius
+      pointer: {
+        length: 0.54, // // Relative to gauge radius
+        strokeWidth: 0.026, // The thickness
+        color: '#ffffff' // Fill color
+      },
+      limitMax: false,     // If false, the max value of the gauge will be updated if value surpass max
+      limitMin: false,     // If true, the min value of the gauge will be fixed unless you set it manually
+      colorStart: '#fe001a',   // Colors
+      colorStop: '#fe001a',    // just experiment with them
+      strokeColor: '#E0E0E0',  // to see which ones work best for you
+      generateGradient: true,
+      highDpiSupport: true     // High resolution support
+    };
+    var target = document.getElementById('gauge'); // your canvas element
+    gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
+    gauge.maxValue = 180; // set max gauge value
+    gauge.setMinValue(0);  // Prefer setter over gauge.minValue = 0
+    gauge.animationSpeed = 30; // set animation speed (32 is default value)
+    gauge.set(140); // set actual value
 
-    console.log("thing");
-    //animate the search (WHY AM I DOING THIS)
-
-    startPlaceholderFadeSequence()
-    setInterval(function(){
-
-      console.log("interval start");
-
-      startPlaceholderFadeSequence()
-
-    }, 9005);
 
   },
   updated: function() {
 
     console.log("updated");
 
-    var elem = document.querySelector('.main-carousel');
-    var flkty = new Flickity( elem, {
-      // options
-      cellAlign: 'left',
-      contain: true
-    });
-
   },
   methods: {
-    selectTab: function(tabIndex){
-      $.each(s.sidebarTabs,function(index,tab){
-        tab.selected = false;
-      });
-      s.sidebarTabs[tabIndex].selected = true;
+    setGauge: function(event){
+
+
+      var p2 = {
+      	x: 188,
+      	y: 293
+      };
+
+      var p1 = {
+      	x: event.center.x,
+      	y: event.center.y
+      };
+
+      // angle in radians
+      var angleRadians = Math.atan2(p2.y - p1.y, p2.x - p1.x);
+
+      // angle in degrees
+      var angleDeg = Math.atan2(p2.y - p1.y, p2.x - p1.x) * 180 / Math.PI;
+
+      gauge.set(angleDeg);
+
     }
   }
 });
-
-
-function startPlaceholderFadeSequence() {
-  setTimeout(function(){
-    $(".search-field").addClass("invisible-placeholder");
-  },2500);
-  setTimeout(function(){
-    $(".search-field").attr("placeholder","Search by Title");
-    $(".search-field").removeClass("invisible-placeholder");
-  },3000);
-  setTimeout(function(){
-    $(".search-field").addClass("invisible-placeholder");
-  },5500);
-  setTimeout(function(){
-    $(".search-field").attr("placeholder","Search by Customer Portfolio");
-    $(".search-field").removeClass("invisible-placeholder");
-  },6000);
-  setTimeout(function(){
-    $(".search-field").addClass("invisible-placeholder");
-  },8500);
-  setTimeout(function(){
-    $(".search-field").attr("placeholder","Search by Name");
-    $(".search-field").removeClass("invisible-placeholder");
-    console.log("sequence end");
-  },9000);
-}
