7fab4310b588563f29c26db40ba281ab45e9bd3f
Aaron Baker <aarbaker@coca-cola.com>
2017-06-27 22:47:40 -0400

    user login?

--- js/auth.js
+++ js/auth.js
@@ -3,7 +3,7 @@ var uiConfig = {
   signInSuccessUrl: '/bragthat',
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
-    // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.EmailAuthProvider.PROVIDER_ID
   ],
   // Terms of service url.
